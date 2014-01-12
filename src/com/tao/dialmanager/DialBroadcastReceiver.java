0599a5150328a76e309e293560e3766222c559bb
Tao Bi <taoobb@gmail.com>
2014-01-12 14:21:35 -0500

    Receive phone call from default dialer

---
+++ src/com/tao/dialmanager/DialBroadcastReceiver.java
@@ -0,0 +1,19 @@
+package com.tao.dialmanager;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class DialBroadcastReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		if (intent.getAction().equals(Intent.ACTION_NEW_OUTGOING_CALL)) {
+			String number = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
+			Log.v("Receiver", number);
+		}
+
+	}
+
+}
